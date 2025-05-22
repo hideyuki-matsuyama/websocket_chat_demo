2727a5065df1bf05d57419f3ea0a54fcd6eff2b4
Hideyuki Matsuyama <choripon@gmail.com>
2025-05-22 23:54:05 +0900

    👍cssを別ファイルに分離＆ルームを実装

--- server.js
+++ server.js
@@ -13,23 +13,17 @@ if (process.env.NODE_ENV !== "production") {
   const livereload = require("livereload");
   const connectLiveReload = require("connect-livereload");
 
-  // public ディレクトリ内の変更を監視する LiveReload サーバーを作成
   const liveReloadServer = livereload.createServer();
   liveReloadServer.watch(__dirname + "/public");
 
-  // LiveReload サーバーがクライアントに接続したときに一度リフレッシュを実行
-  // (サーバー起動時に既に開いているページをリロードするため)
   liveReloadServer.server.once("connection", () => {
     setTimeout(() => {
       liveReloadServer.refresh("/");
     }, 100);
   });
-
-  // connect-livereload ミドルウェアを Express アプリケーションに適用
   app.use(connectLiveReload());
 }
 
-// 静的ファイルを提供
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
@@ -39,13 +33,86 @@ app.get("/", (req, res) => {
 io.on("connection", (socket) => {
   console.log("新しいユーザーが接続しました:", socket.id);
 
-  socket.on("chat message", (msg) => {
-    console.log("メッセージ:", msg);
-    io.emit("chat message", msg);
+  // 現在のソケットがどのルームにいるかを保持する変数
+  socket.currentRoom = null;
+
+  // ルーム参加処理
+  socket.on("join room", (roomName, callback) => {
+    if (socket.currentRoom) {
+      // 既に他のルームに参加している場合は、まず退出させる
+      socket.leave(socket.currentRoom);
+      io.to(socket.currentRoom).emit(
+        "room notification",
+        `ユーザー ${socket.id.substring(0, 5)}... がルーム '${
+          socket.currentRoom
+        }' から退出しました。`
+      );
+      console.log(
+        `ユーザー ${socket.id} がルーム '${socket.currentRoom}' から退出しました。`
+      );
+    }
+
+    socket.join(roomName);
+    socket.currentRoom = roomName;
+    console.log(
+      `ユーザー ${socket.id} がルーム '${roomName}' に参加しました。`
+    );
+
+    // 参加した本人に成功を通知
+    if (callback && typeof callback === "function") {
+      callback({ success: true, message: `ルーム '${roomName}' にようこそ！` });
+    }
+
+    // ルーム内の他の参加者に通知 (本人以外)
+    socket
+      .to(roomName)
+      .emit(
+        "room notification",
+        `ユーザー ${socket.id.substring(
+          0,
+          5
+        )}... がルーム '${roomName}' に参加しました。`
+      );
+    // 参加した本人にもシステムメッセージとして送信（オプション）
+    socket.emit(
+      "room notification",
+      `あなたはルーム '${roomName}' に参加しました。`
+    );
   });
 
+  // メッセージ受信とルーム内への転送
+  socket.on("chat message", (data) => {
+    // data は { room: "ルーム名", message: "メッセージ内容" } という形式を期待
+    if (data && data.room && data.message && socket.currentRoom === data.room) {
+      console.log(
+        `ルーム '${data.room}' のメッセージ (from ${socket.id}): ${data.message}`
+      );
+      // 送信者情報も付加して、ルーム内の全員（送信者含む）にメッセージを送信
+      io.to(data.room).emit("chat message", {
+        message: data.message,
+        sender: socket.id, // 送信者のID
+        room: data.room,
+      });
+    } else {
+      console.log("無効なメッセージまたはルーム情報:", data);
+    }
+  });
+
+  // 切断処理
   socket.on("disconnect", () => {
     console.log("ユーザーが切断しました:", socket.id);
+    if (socket.currentRoom) {
+      // ユーザーが参加していたルームがあれば、そのルームの他の参加者に退出を通知
+      io.to(socket.currentRoom).emit(
+        "room notification",
+        `ユーザー ${socket.id.substring(0, 5)}... がルーム '${
+          socket.currentRoom
+        }' から退出しました。`
+      );
+      console.log(
+        `ユーザー ${socket.id} がルーム '${socket.currentRoom}' から退出しました。`
+      );
+    }
   });
 });
 
